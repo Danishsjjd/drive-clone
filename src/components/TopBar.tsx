6799a688c07841c8ef3fc19c35abd633e36eac18
DanishSjjd <danishsjjad@gmail.com>
2023-01-04 19:41:53 +0500

    fix: inputDialog to two separate component Input & Dialog

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,10 +1,46 @@
+import { addDoc, serverTimestamp } from "firebase/firestore"
 import { MouseEventHandler, useState } from "react"
-import { AiFillFolderAdd, AiFillFileAdd } from "react-icons/ai"
+import { useForm } from "react-hook-form"
+import { AiFillFileAdd, AiFillFolderAdd } from "react-icons/ai"
 import { IconType } from "react-icons/lib/esm/iconBase"
-import InputDialog from "./form/InputDialog"
+
+import { db } from "../config/firebase"
+import { useAppSelector } from "../hooks/hooks"
+import { getUserCredential } from "../store/auth"
+import Dialog from "./Dialog"
+import Input from "./form/Input"
+
+type FormData = {
+  name: string
+}
 
 const TopBar = () => {
+  const userData = useAppSelector(getUserCredential)
   const [isInputDialogOpen, setIsInputDialogOpen] = useState(false)
+
+  const {
+    formState: { errors },
+    register,
+    reset,
+    handleSubmit,
+  } = useForm<FormData>()
+
+  const submit = async ({ name }: FormData) => {
+    try {
+      await addDoc(db.folders(), {
+        name,
+        userId: userData?.uid as string,
+        parentId: null,
+        createdAt: serverTimestamp(),
+        path: [],
+      })
+      setIsInputDialogOpen(false)
+      reset()
+    } catch (e) {
+      console.log("error is", e)
+    }
+  }
+
   return (
     <div className="mx-auto flex max-w-7xl justify-between py-2 px-3">
       <BreadCrumb />
@@ -17,10 +53,29 @@ const TopBar = () => {
         />
       </div>
 
-      <InputDialog
+      <Dialog
         isOpen={isInputDialogOpen}
         setIsOpen={setIsInputDialogOpen}
-      />
+        title={"Enter Folder Name"}
+      >
+        <form className="mt-2" onSubmit={handleSubmit(submit)}>
+          <Input<FormData>
+            title=""
+            errors={errors}
+            name="name"
+            register={register}
+            type="text"
+          />
+          <div className="mt-4">
+            <button
+              type="button"
+              className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+            >
+              Create
+            </button>
+          </div>
+        </form>
+      </Dialog>
     </div>
   )
 }
